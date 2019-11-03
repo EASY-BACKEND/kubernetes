5784f9435aed94851941e6dc935d77220990d087
SamThicker <704851103@qq.com>
2019-11-03 16:49:06 +0800

    Add files via upload

---
+++ generateSSL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -key ca.key -out ca.crt -days 3650 -subj "/C=CN/ST=HB/L=WH/O=DM/OU=YPT/CN=CA"
+openssl genrsa -out dashboard.key 2048
+openssl req -new -sha256 -key dashboard.key -out dashboard.csr -subj "/C=CN/ST=HB/L=WH/O=DM/OU=YPT/CN=$1"
+echo "extensions = san
+[san]
+keyUsage = digitalSignature
+extendedKeyUsage = clientAuth,serverAuth
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+subjectAltName = IP:$1,IP:127.0.0.1,DNS:$1,DNS:localhost
+" > dashboard.cnf
+openssl x509 -req -sha256 -days 3650 -in dashboard.csr -out dashboard.crt -CA ca.crt -CAkey ca.key -CAcreateserial -extfile dashboard.cnf
+rm -f dashboard.csr dashboard.cnf ca.srl ca.key
+echo "create completed"
